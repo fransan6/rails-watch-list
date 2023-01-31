5cb45f5de28b27ec20059541b4fa636ca8df72ce
francesca sandford <fran6san@gmail.com>
2023-01-31 21:13:47 +0000

    challenge update

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,6 +3,3 @@
 // ./bin/rails generate stimulus controllerName
 
 import { application } from "./application"
-
-import TomSelectController from "./tom_select_controller.js"
-application.register("tom-select", TomSelectController)
