54ebda741de83ef091620006a5fece2dbd05365a
francesca sandford <fran6san@gmail.com>
2023-02-02 16:49:38 +0000

    challenge update

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,11 @@
 import "@hotwired/turbo-rails";
 import "./controllers";
 import "bootstrap";
+
+console.log('This is connected');
+
+// const title = document.getElementById('cat')
+// console.log(title)
+// title.addEventListener('click', (event) => {
+//   event.currentTarget.classList.toggle("redtext");
+// })
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,3 +3,6 @@
 // ./bin/rails generate stimulus controllerName
 
 import { application } from "./application"
+
+import TomSelectController from "./tom_select_controller"
+application.register("tom-select", TomSelectController)
